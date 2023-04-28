ec4f4b76acb7734aa0ab257f95c3764dbeddc8d0
Laura Ramos <marialauraramos.5@gmail.com>
2023-04-28 17:35:59 -0300

    (feat): connect db container with server and initialize db user

--- install.sh
+++ install.sh
@@ -1,14 +1,28 @@
-#MONGO_INITDB_ROOT_USERNAME=
-#MONGO_INITDB_ROOT_PASSWORD=
-#MONGO_INITDB_DATABASE=my_database
-#NODE_ENV=development
-
 configs="MONGO_INITDB_DATABASE=wikideas\nNODE_ENV=development"
 
-echo "Input MongoDB root username:"
+echo "Input MongoDB root username (defaults to root):"
 read mongodb_root_username
+if [ -z "$mongodb_root_username" ]; then
+  mongodb_root_username="wikideas"
+fi
 
-echo "Input MongoDB root password:"
+echo "Input MongoDB root password (defaults to root):"
 read mongodb_root_password
+if [ -z "$mongodb_root_password" ]; then
+  mongodb_root_password="wikideas"
+fi
 
-echo -e "MONGO_INITDB_ROOT_USERNAME=$mongodb_root_username\nMONGO_INITDB_ROOT_PASSWORD=$mongodb_root_password\n$configs" > .env.dev
+echo "Input wikideas user (defaults to wikideas):"
+read mongodb_username;
+if [ -z "$mongodb_username" ]; then
+  mongodb_username="wikideas"
+fi
+
+echo "Input wikideas password (defaults to wikideas):"
+read mongodb_password;
+if [ -z "$mongodb_password" ]; then
+  mongodb_password="wikideas"
+fi
+
+
+echo -e "MONGO_INITDB_ROOT_USERNAME=$mongodb_root_username\nMONGO_INITDB_ROOT_PASSWORD=$mongodb_root_password\MONGO_USERNAME=$mongodb_username\nMONGO_PASSWORD=$mongodb_password\n$configs" > .env.dev
